a65a78d391b8580867430ec9cfa2e6c6709b5120
conicole <conicole@insa-rennes.fr>
2014-12-09 11:43:23 +0100

    [prolol] debut tp10

--- prolog/tp9/bin.pl
+++ prolog/tp9/bin.pl
@@ -19,19 +19,6 @@ creer_binomes(_,[],[]).
 creer_binomes(B1,[T|Q],[(B1,T)|Cbin]):-
 	creer_binomes(B1,Q,Cbin).
 
-/* TESTS
-
-[eclipse 2]: combiner (["valou","maud","corentin","jessica parker","hoel","flobear","justin bieber"],L).
-
-L = [("valou", "maud"), ("valou", "corentin"), ("valou", "jessica parker"), ("valou", "hoel"), ("valou", "flobear"), ("valou", "justin bieber"), ("maud", "corentin"), ("maud", "jessica parker"), ("maud", "hoel"), ("maud", "flobear"), ("maud", "justin bieber"), ("corentin", "jessica parker"), ("corentin", "hoel"), ("corentin", "flobear"), ("corentin", "justin bieber"), ("jessica parker", "hoel"), ("jessica parker", "flobear"), ("jessica parker", "justin bieber"), (..., ...), ...]
-Yes (0.00s cpu)
-[eclipse 3]: combiner (["valou","maud","corentin"],L).
-
-[eclipse 4]: combiner (["valou","maud","corentin","flobear"],L).
-
-L = [("valou", "maud"), ("valou", "corentin"), ("valou", "flobear"), ("maud", "corentin"), ("maud", "flobear"), ("corentin", "flobear")]
-
-*/
 % Question 1.2 %
 
 check_acc(_,[]).
@@ -63,15 +50,6 @@ extraire_aux([(B1,B2)|Q], NbBinomes, TP, [(B1,B2)|RemainingBinomes], Acc ):-
 extraire(AllPossibleBinomes, NbBinomes, TP, RemainingBinomes ):-
 	extraire_aux(AllPossibleBinomes, NbBinomes, TP, RemainingBinomes, [] ).
 	
-/* TESTS
-
-[eclipse 49]: combiner(["valou","maud","coco","flobear","hoel","justin"],Binomes),extraire(Binomes,2,TP,R).
-
-Binomes = [("valou", "maud"), ("valou", "coco"), ("valou", "flobear"), ("valou", "hoel"), ("valou", "justin"), ("maud", "coco"), ("maud", "flobear"), ("maud", "hoel"), ("maud", "justin"), ("coco", "flobear"), ("coco", "hoel"), ("coco", "justin"), ("flobear", "hoel"), ("flobear", "justin"), ("hoel", "justin")]
-TP = [("coco", "flobear"), ("valou", "maud")]
-R = [("valou", "coco"), ("valou", "flobear"), ("valou", "hoel"), ("valou", "justin"), ("maud", "coco"), ("maud", "flobear"), ("maud", "hoel"), ("maud", "justin"), ("coco", "hoel"), ("coco", "justin"), ("flobear", "hoel"), ("flobear", "justin"), ("hoel", "justin")]
-*/
-
 % Question 1.3 %
 
 les_tps(Copains, Tps) :-
@@ -92,10 +70,100 @@ longueur_liste([_|Q],Res):-
 	longueur_liste(Q,R2),
 	Res is R2+1.
 	
-/* TESTS
+/*
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%           TEST               %%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% Question 1.1 %
+
+
+[eclipse 2]: combiner (["valou",
+		"maud",
+		"corentin",
+		"jessica parker",
+		"hoel",
+		"flobear",
+		"justin bieber"],
+		L).
+
+L = [("valou", "maud"),
+	("valou", "corentin"),
+	("valou", "jessica parker"),
+	("valou", "hoel"),
+	("valou", "flobear"),
+	("valou", "justin bieber"),
+	("maud", "corentin"),
+	("maud", "jessica parker"),
+	("maud", "hoel"),
+	("maud", "flobear"),
+	("maud", "justin bieber"),
+	("corentin", "jessica parker"),
+	("corentin", "hoel"),
+	("corentin", "flobear"),
+	("corentin", "justin bieber"),
+	("jessica parker", "hoel"),
+	("jessica parker", "flobear"),
+	("jessica parker", "justin bieber"),
+	(..., ...), ...]
+
+[eclipse 4]: combiner (["valou","maud","corentin","flobear"],L).
+
+L = [("valou", "maud"),
+	("valou", "corentin"),
+	("valou", "flobear"),
+	("maud", "corentin"),
+	("maud", "flobear"),
+	("corentin", "flobear")]
+
+
+
+% Question 1.2 %
+
+[eclipse 49]: 	combiner(["valou","maud","coco","flobear","hoel","justin"],
+			Binomes),
+			extraire(Binomes,2,TP,R).
+
+Binomes = [("valou", "maud"), 
+	("valou", "coco"),
+ 	("valou", "flobear"),
+  	("valou", "hoel"),
+   	("valou", "justin"),
+   	("maud", "coco"),
+ 	("maud", "flobear"),
+ 	("maud", "hoel"),
+ 	("maud", "justin"),
+  	("coco", "flobear"),
+  	("coco", "hoel"),
+	("coco", "justin"),
+	("flobear", "hoel"),
+	("flobear", "justin"),
+	("hoel", "justin")]
+
+TP = [("coco", "flobear"),
+	("valou", "maud")]
+
+R = [("valou", "coco"),
+	("valou", "flobear"),
+	("valou", "hoel"),
+	("valou", "justin"),
+	("maud", "coco"),
+	("maud", "flobear"),
+	("maud", "hoel"),
+	("maud", "justin"),
+	("coco", "hoel"),
+	("coco", "justin"),
+	("flobear", "hoel"),
+	("flobear", "justin"),
+	("hoel", "justin")]
+
 
 [eclipse 5]: les_tps(["coco","valou","maud","flobear"],Tps).
 
-Tps = [[("maud", "flobear"), ("coco", "valou")], [("valou", "flobear"), ("coco", "maud")], [("valou", "maud"), ("coco", "flobear")]]
+Tps = [[("maud", "flobear"),("coco", "valou")],
+	[("valou", "flobear"),("coco", "maud")],
+	[("valou", "maud"),("coco", "flobear")]]
+
 
 */
