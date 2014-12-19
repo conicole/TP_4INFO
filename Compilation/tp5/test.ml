887bb98109a500aca78b2402a83510ad89436b35
conicole <corentin.nicole@insa-rennes.fr>
2014-12-19 14:26:16 +0100

    [Compilo] last report tp5

---
+++ Compilation/tp5/test.ml
@@ -0,0 +1,20 @@
+
+# 1+1;;
+- = 2:int
+# let x = 2;;
+# x;;
+- = 2:int
+# x+3;;
+- = 5:int
+# x < 5;;
+- = true:bool
+# false;;
+- = false:bool
+# let x = 3 in x + 2;;
+- = 5:int
+# let x = 3 in x + 2;;
+- = 5:int
+# fst(5,false);;
+- = 5:int
+# 5+false;;
+ error: operation illegal
